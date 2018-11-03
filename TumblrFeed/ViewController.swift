cfb1e2cb64aeb4f8ab0e460e470683e40ceed895
Eli Armstrong MACBOOKPRO <ELIJEFFREYARMSTRONG@GMAIL.COM>
2018-11-03 10:53:17 -0700

    Revert "added pods, tableView, linked backend with table view, created tableView cell"
    This reverts commit d1fdf89eb227de15a4ed43ea2829b7dc1d40642a.

---
+++ TumblrFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TumblrFeed
+//
+//  Created by Eli Armstrong on 9/2/18.
+//  Copyright © 2018 Eli Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
